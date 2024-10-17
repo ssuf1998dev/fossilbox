6718eb4767606a6bbd7d528bb60fdab993719ea8
ssuf1998dev <ssuf1998dev@126.com>
2024-10-17 23:39:43 +0800

    fix: type fix

--- utils/config-form-env.ts
+++ utils/config-form-env.ts
@@ -1,14 +1,12 @@
 import pathSet from "lodash-es/set";
 import process from "node:process";
 
-import type { UserConfig } from "../server";
-
 export default () => Object.entries({
   FOSSILBOX_HOST: "host",
   FOSSILBOX_PORT: "port",
   FOSSILBOX_DB: "db",
   FOSSILBOX_SQLITE_FILE: "sqlite.file",
-}).reduce((map: UserConfig, [key, value]) => {
+}).reduce((map, [key, value]) => {
   const env = process.env[key];
   !!env && pathSet(map, value, env);
   return map;
